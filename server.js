c3058c8e2e35cc5a834c57774dd01b7e3062ced9
Mostafa Zakaria <80599839+Mostafaelfekky@users.noreply.github.com>
2021-06-11 16:14:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const FAVORTIE_INSTRUCTOR = process.env.NAME;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,6 +6,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
+        console.log(`${FAVORTIE_INSTRUCTOR} rocks!`);
     console.log("Microservices rock!");
     await sleep(5000);
   }
